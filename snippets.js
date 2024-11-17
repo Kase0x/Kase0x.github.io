a09be162f24f3590f4438d310f53ae4398048dd6
Kase0x <120433810+Kase0x@users.noreply.github.com>
2024-11-17 15:11:52 -0800

    Initial

---
+++ snippets.js
@@ -0,0 +1,22 @@
+// <![CDATA[
+        function loading(){
+            $("#loading").show();
+            $("#content").hide();   
+              
+        };
+// ]]>
+
+// $(document).ready(function() { $("#pageFade").fadeIn(500) });
+
+// function fadeinFaq() { console.log("hit");
+// $("#pageindex").addClass('hidden');
+// $("#pageindex").fadeIn(1000) };
+
+// function fadeinIndex() { console.log("hit");
+// $("#pageindex").addClass('hidden') ;
+// $("#pageindex").fadeIn(1000)};
+
+
+
+$(document).ready(function() { $("#fadeone").fadeIn(500) });
+// $(window).load(function(){$("#pageFade").fadeIn(1000)});
